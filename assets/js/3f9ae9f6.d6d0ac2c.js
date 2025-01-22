70ff3f1a48549dd9f122c72fbdaaa8e1e9cb4b3d
jg23497 <jg23497@gmail.com>
2025-01-22 18:16:33 +0000

    Deploy website - based on c6e511fa1a6bf26cc1c35c02399db07997c36826

---
+++ assets/js/3f9ae9f6.d6d0ac2c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkjgulan=self.webpackChunkjgulan||[]).push([[32],{8322:n=>{n.exports=JSON.parse('{"authors":[{"name":"J. Gulan","title":"Software Engineer","socials":{"linkedin":"https://www.linkedin.com/in/jgulan/","github":"https://github.com/jg23497"},"imageURL":"https://github.com/jg23497.png","key":"jgulan","page":null,"count":0}]}')}}]);
